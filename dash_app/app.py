85fca9bb2a1016015c0f3d6a9054b84e3a8f3023
copycat <yehan_PPU@126.com>
2019-09-15 14:50:28 +0800

    dash app plan start

--- dash_app/app.py
+++ dash_app/app.py
@@ -8,45 +8,67 @@ from dash.dependencies import Input, Output, State
 app = dash.Dash(__name__)
 
 app.layout = html.Div([
-    html.Div(className='container', children=[
 
+    # 标题栏
+    html.Nav(className='navbar navbar-expand-lg navbar-dark bg-dark', children=[
+
+        html.A(className='navbar-brand', children=[
+            "现场勘查知识图谱探索系统"
+        ]),
+        html.Div(className='collapse navbar-collapse', children=[
+            html.Form(className='', children=[
+                html.Button(className='btn btn-outline-success', children=[
+                    "Contact us"
+                ])
+            ])
+        ])
+    ]),
+
+    html.Div(className='container', children=[
         # 设置中心
         html.Div(className='row', children=[
-           html.Div(className='col-6', children=[
-               dcc.Slider(
-                   id='number_of_main_node',
-                   min=3,
-                   max=100,
-                   step=1,
-                   value=10,
-                   marks={value: value for x, value in enumerate(range(1, 100, 10))}
-               )
-           ]),
+            html.Div(className='col-6', children=[
+                dcc.Slider(
+                    id='number_of_main_node',
+                    min=3,
+                    max=100,
+                    step=1,
+                    value=10,
+                    marks={value: value for x, value in enumerate(range(1, 100, 10))}
+                )
+            ]),
 
-           html.Div(className='col-6', children=[
-              dcc.Input(
-                  id='search_keyword',
-                  placeholder='Enter a keyword...',
-                  type='text',
-                  value=''
-              ),
+            html.Div(className='col-6', children=[
+                dcc.Input(
+                    id='search_keyword',
+                    placeholder='Enter a keyword...',
+                    type='text',
+                    value=''
+                ),
 
-              html.Button('开始搜索', id='start_button')
-           ]),
+                html.Button('开始搜索', id='start_button')
+            ])
+        ]),
 
-           html.Div(className='row', children=[
-               html.Div(className='col-6', children=[
-                   dcc.Dropdown(
-                       id='layout_select',
-                       value='random',
-                       clearable=False,
-                       options=[
-                           {'label': name.capitalize(), 'value': name}
-                           for name in ['grid', 'random', 'circle', 'cose', 'concentric']
-                       ]
-                   )
-               ])
-           ])
+        html.Div(className='row', children=[
+            html.Div(className='col-6', children=[
+                dcc.Dropdown(
+                    id='layout_select',
+                    value='random',
+                    clearable=False,
+                    options=[
+                        {'label': name.capitalize(), 'value': name}
+                        for name in ['grid', 'random', 'circle', 'cose', 'concentric']
+                    ]
+                )
+            ])
+        ]),
+
+
+        html.Div(className='row', children=[
+            html.Div(className='col-6', children=[
+                html.P(id='graph-click-edge-output')
+            ])
         ]),
 
         # 接下来是绘图区域
@@ -56,8 +78,27 @@ app.layout = html.Div([
                     id='cytoscape-basic',
                     layout={'name': 'cose'},
                     style={'width': '100%', 'height': '1000px'},
-                    elements=[
-
+                    elements=[],
+                    stylesheet=[
+                        {
+                            'selector': 'node',
+                            'style': {
+                                'content': 'data(label)'
+                            }
+                        },
+                        {
+                            'selector': '.node',
+                            'style': {
+                                'background-color': 'blue',
+                                'line-color': 'gary'
+                            }
+                        },
+                        {
+                            'selector': '.node_main',
+                            'style': {
+                                'shape': 'rectangle'
+                            }
+                        }
                     ]
                 )
             ])
@@ -70,14 +111,13 @@ app.layout = html.Div([
 @app.callback(
     Output('cytoscape-basic', 'elements'),
     [Input('start_button', 'n_clicks'),
-     Input('layout_select', 'value')],
+     Input('number_of_main_node', 'value')],
     [State('search_keyword', 'value')]
 )
-def extract_data_from_neo4j(n_clicks, layout, value):
-
+def extract_data_from_neo4j(n_clicks, limit, value):
     # 此部分为核心作图数据获取
     driver = Neo4jOperator()  # 初始化数据库驱动
-    node_result, link_result = driver.search_data_normal(value)  # 调用驱动提取数据
+    node_result, link_result = driver.search_data_normal(value, limit)  # 调用驱动提取数据
 
     nodes = list()
     for _, node_dict in enumerate(node_result):
@@ -87,12 +127,13 @@ def extract_data_from_neo4j(n_clicks, layout, value):
             # 具体参见 https://dash.plot.ly/cytoscape/styling
         }
         nodes.append(tmp_node)  # 之所以要用与下面的不一样的方法，是因为这里用到的enumerate是一个迭代器。并非一个完整的list
+
     # edges = list()
     edges = [
         {
             'data': {'source': link_dict['source'],
                      'target': link_dict['target'],
-                     'label': link_dict['edge']
+                     'label': link_dict['edge_description']
                      }
         }
         for link_dict in link_result
@@ -100,3 +141,12 @@ def extract_data_from_neo4j(n_clicks, layout, value):
     elements = nodes + edges
 
     return elements
+
+
+@app.callback(
+    Output('graph-click-edge-output', 'children'),
+    [Input('cytoscape-basic', 'tapEdgeData')]
+)
+def display_edge_data(data):
+    if data:
+        return data['label']
